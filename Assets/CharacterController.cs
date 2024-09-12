d77fd690f8430141becbe52b52d7a9f7f8105648
Jun Shimura <j.shimura@sugawara.ac.jp>
2024-09-12 17:34:10 +0900

    CharacterController

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public enum CharacterType {　// キャラクターのタイプ
+        player, 
+        monster, 
+        citizen , 
+        animal
+    }
+    public CharacterType type;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (type == CharacterType.player)
+        {
+            //　Playerの処理
+        }
+        else if (type == CharacterType.monster) {
+            //　Monsterの処理
+        }
+        if (type == CharacterType.citizen|| type== CharacterType.player)
+        {
+            // 人型の処理
+        }
+    }
+}
